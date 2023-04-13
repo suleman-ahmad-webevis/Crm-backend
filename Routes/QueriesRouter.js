b9568884ce350b1c7a311aee2077a049816633f7
unknown <ismailzeeshan082@gmail.com>
2023-04-13 15:32:54 +0500

    fixed query route

--- Routes/QueriesRouter.js
+++ Routes/QueriesRouter.js
@@ -1,8 +1,87 @@
 const express = require('express');
 const QueriesRouter = express.Router()
 const Queries = require('../Models/Queries')
+const middleware = require("../middleware");
 
 
+// Create a queries
+QueriesRouter.post('/', middleware.isAdmin, async (req, res, next) => {
+  const { name, company, email, } = req.body;
+
+  const queriesData = new Queries({
+    name,
+    company,
+    email,
+  });
+
+  queriesData.save()
+    .then((queries) => {
+      res.status(200).json({ success: true, message: "Query is created successfully" });
+    }, (err) => next(err))
+    .catch((err) => {
+      next(err);
+    });
+});
+
+// Get all queries
+QueriesRouter.get('/', middleware.isAdmin, async (req, res, next) => {
+  Queries.find().populate("developer")
+    .then((queries) => {
+      res.status(200).json({ success: true, data: queries });
+    }, (err) => next(err))
+    .catch((err) => {
+      next(err);
+    });
+});
+
+// Get a specific queries by ID
+QueriesRouter.get('/:id', middleware.isAdmin, async (req, res, next) => {
+  const { id } = req.params;
+
+  Queries.findById(id).populate("developer")
+    .then((queries) => {
+      if (!queries) {
+        return res.status(404).json({ message: 'queries not found' });
+      }
+      res.status(200).json({ success: true, data: queries });
+    }, (err) => next(err))
+    .catch((err) => {
+      next(err);
+    });
+});
+
+// Update a queries by ID
+QueriesRouter.patch('/:id', middleware.isAdmin, async (req, res, next) => {
+  try {
+    const { developer, ...rest } = req.body;
+    const user = await Queries.findByIdAndUpdate(
+      req.params.id,
+      { $push: { developer }, $set: rest },
+      { new: true, useFindAndModify: false }
+    );
+    res.status(200).json({ msg: "User updated successfully." });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Delete a queries by ID
+QueriesRouter.delete('/:id', middleware.isAdmin, async (req, res, next) => {
+  const { id } = req.params;
+
+  Queries.findByIdAndDelete(id)
+    .then((queries) => {
+      if (!queries) {
+        return res.status(404).json({ message: 'queries not found' });
+      }
+      res.json({ success: true, message: "Query Deleted" });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+// Read CSV
 QueriesRouter.post('/read-csv', (req, res, next) => {
   if (!req.file) {
       return res.status(400).json({ error: 'No file uploaded' });
@@ -34,7 +113,7 @@ QueriesRouter.post('/read-csv', (req, res, next) => {
   fs.createReadStream(req.file.path).pipe(parser);
 });
 // send email
-QueriesRouter.post("/sendEmail", async (req, res, next) => {
+QueriesRouter.post("/sendEmail", middleware.isAdmin, async (req, res, next) => {
   const emailList = req.body.emails;
 
   for (let i = 0; i < emailList.length; i++) {
@@ -64,84 +143,6 @@ QueriesRouter.post("/sendEmail", async (req, res, next) => {
   }
 
   res.status(200).send('Welcome emails sent successfully!');
-})
-
-
-// Create a queries
-QueriesRouter.post('/', (req, res, next) => {
-  const { name, company, email, } = req.body;
-
-  const queriesData = new Queries({
-    name,
-    company,
-    email,
-  });
-
-  queriesData.save()
-    .then((queries) => {
-      res.status(201).json(queries);
-    })
-    .catch((err) => {
-      next(err);
-    });
-});
-
-// Get all queries
-QueriesRouter.get('/', (req, res, next) => {
-  Queries.find().populate("developer")
-    .then((queries) => {
-      res.status(200).json(queries);
-    })
-    .catch((err) => {
-      next(err);
-    });
-});
-
-// Get a specific queries by ID
-QueriesRouter.get('/:id', (req, res, next) => {
-  const { id } = req.params;
-
-  Queries.findById(id).populate("developer")
-    .then((queries) => {
-      if (!queries) {
-        return res.status(404).json({ message: 'queries not found' });
-      }
-      res.status(200).json(queries);
-    })
-    .catch((err) => {
-      next(err);
-    });
-});
-
-// Update a queries by ID
-QueriesRouter.patch('/:id', async (req, res, next) => {
-  try {
-    const { developer, ...rest } = req.body;
-    const user = await Queries.findByIdAndUpdate(
-      req.params.id,
-      { $push: { developer }, $set: rest },
-      { new: true, useFindAndModify: false }
-    );
-    res.status(200).json({ msg: "User updated successfully.", user });
-  } catch (err) {
-    next(err);
-  }
-});
-
-// Delete a queries by ID
-QueriesRouter.delete('/:id', (req, res, next) => {
-  const { id } = req.params;
-
-  Queries.findByIdAndDelete(id)
-    .then((queries) => {
-      if (!queries) {
-        return res.status(404).json({ message: 'queries not found' });
-      }
-      res.status(200).json(queries);
-    })
-    .catch((err) => {
-      next(err);
-    });
-});
+}) 
 
 module.exports = QueriesRouter;
