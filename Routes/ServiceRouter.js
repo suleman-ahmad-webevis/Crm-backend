b2b559e09d9d419eebec057644a845a88e443870
unknown <ismailzeeshan082@gmail.com>
2023-04-13 17:01:03 +0500

    fixed service route

--- Routes/ServiceRouter.js
+++ Routes/ServiceRouter.js
@@ -1,85 +1,73 @@
 const express = require('express');
 const ServiceRouter = express.Router();
-const Service = require('../models/Servics');
+const Service = require('../Models/Service');
 
-// Create a service
-ServiceRouter.post('/', (req, res, next) => {
-  const { name, email, service, price } = req.body;
+// Get all Service
+ServiceRouter.get('/',  async (req, res, next) => {
+  try {
+    Service.find()
+    .then((Service) => {
+      res.status(200).json({ success: true, data: Service });
+    }, (err) => next(err))
+  } catch (err) {
+    console.log("er", err)
+    next(err)
+  }
+});
 
-  const serviceData = new Service({
-    name,
-    email,
-    service,
-    price
+// Get a specific deal by ID
+ServiceRouter.get('/:id',  async (req, res, next) => {
+  try {
+    Service.findById(req.params.id)
+      .then((Service) => {
+        res.status(200).json({ success: true, data: Service });
+      }, (err) => next(err))
+  } catch (err) {
+    next(err)
+  }
+});
+
+// Create a new deal
+ServiceRouter.post('/',  async (req, res, next) => {
+  const deal = new Service({
+    name: req.body.name,
+    email: req.body.email,
+    service: req.body.service,
+    price: req.body.price,
   });
 
-  serviceData.save()
-    .then((service) => {
-      res.status(201).json(service);
-    })
-    .catch((err) => {
-      next(err);
-    });
+  try {
+    deal.save()
+    .then(() => {
+      res.status(200).json({ success: true, message: "Service is created successfully" });
+    }, (err) => next(err))
+} catch (err) {
+  next(err)
+}
 });
 
-// Get all services
-ServiceRouter.get('/', (req, res, next) => {
-  Service.find()
-    .then((services) => {
-      res.status(200).json(services);
-    })
-    .catch((err) => {
-      next(err);
-    });
+// Update a deal
+ServiceRouter.patch('/:id',  async (req, res, next) => {
+  Service.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
+    .then(() => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json("Service Updated Successfully.");
+    }, (err) => next(err))
+    .catch((err) => next(err));
 });
 
-// Get a specific service by ID
-ServiceRouter.get('/:id', (req, res, next) => {
-  const { id } = req.params;
+// Delete a deal
 
-  Service.findById(id)
-    .then((service) => {
-      if (!service) {
-        return res.status(404).json({ message: 'Service not found' });
-      }
-      res.status(200).json(service);
-    })
-    .catch((err) => {
-      next(err);
-    });
+ServiceRouter.delete('/:id',  async (req, res, next) => {
+  Service.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({ success: true, message: "Service Deleted" });
+    }, (err) => next(err))
+    .catch((err) => next(err));
 });
 
-// Update a service by ID
-ServiceRouter.put('/:id', (req, res, next) => {
-  const { id } = req.params;
-  const { name, email, service, price } = req.body;
-
-  Service.findByIdAndUpdate(id, { name, email, service, price }, { new: true })
-    .then((service) => {
-      if (!service) {
-        return res.status(404).json({ message: 'Service not found' });
-      }
-      res.status(200).json(service);
-    })
-    .catch((err) => {
-      next(err);
-    });
-});
-
-// Delete a service by ID
-ServiceRouter.delete('/:id', (req, res, next) => {
-  const { id } = req.params;
-
-  Service.findByIdAndDelete(id)
-    .then((service) => {
-      if (!service) {
-        return res.status(404).json({ message: 'Service not found' });
-      }
-      res.status(200).json(service);
-    })
-    .catch((err) => {
-      next(err);
-    });
-});
 
 module.exports = ServiceRouter;
